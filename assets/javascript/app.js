d0b31f9c5b3eeb606f129517187633155da7665c
Bossman8 <jones.boston8@gmail.com>
2019-08-06 01:11:18 -0600

    added to style, js and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+    
+function displayWeatherInfo() {
+    var city = $("#cityName").val();
+    var apiKey = "b1ea2298caabef8f64aebd0a4fb8bfab"
+    var city = "Boston"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID="+apiKey+"&units=imperial&cnt=5"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
+};
+displayWeatherInfo();
